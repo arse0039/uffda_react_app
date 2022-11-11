535ab2e5c7607ceb98d787fa4ee319d3484b3392
David Claphan <claphand@flip2.engr.oregonstate.edu>
2022-11-10 20:34:26 -0800

    updated host / user / password info

--- server/db_config.js
+++ server/db_config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 const db = mysql.createConnection({
     connectionLimit : 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'uffda_test'
+    host: 'classmysql.engr.oregonstate.edu',
+    user: 'cs340_claphand',
+    password: '6780',
+    database: 'cs340_claphand'
 });
 
 module.exports = db;
