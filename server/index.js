dd7acc8eb9d52150639c688a2f050c37dfd6a6c4
David Claphan <claphand@flip2.engr.oregonstate.edu>
2022-11-13 11:33:07 -0800

    recommitting Location updates

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ app.use(cors());
 
 const PORT = 10725;
 
-// View volunteer data for populating Table
+/************* GET *************/
+
+// View Volunteers data for populating table
 app.get("/volunteerData", (req,res)=>{
     const volunteerSelect = 'SELECT * FROM Volunteers';
     db.query(volunteerSelect, (err, result)=>{
@@ -17,6 +19,17 @@ app.get("/volunteerData", (req,res)=>{
     });
 });
 
+// View Locations data for populating table
+app.get("/locationData", (req, res) => {
+    const locationSelect = 'SELECT * FROM Locations';
+    db.query(locationSelect, (err, result) => {
+        if(err) {return result.json(err)}
+        return res.json(result)
+    });
+});
+
+
+/************* POST *************/
 // Insert into Volunteer Table
 app.post("/volunteersInsert", (req, res) => {
     const name = req.body.name;
@@ -30,7 +43,21 @@ app.post("/volunteersInsert", (req, res) => {
     });
 })
 
-// Update Volunteer Table item
+// Insert into Location Table
+app.post("/locationsInsert", (req, res) => {
+    const name = req.body.name;
+    const address = req.body.address;
+
+    const locInsert = "INSERT INTO Locations (name, address) VALUES (?, ?)"
+    db.query(locInsert, [name, address], (err, result)=> {
+        if(err){return result.json(err)}
+        return res.json(result)
+    });
+})
+
+
+/************* PUT *************/
+// Update Volunteer Table record
 app.put("/volunteers/:id", (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
@@ -44,7 +71,22 @@ app.put("/volunteers/:id", (req, res) => {
     });
 });
 
-// Delete Volunteer Table item
+// Update Location Table record
+app.put("/locations/:id", (req, res) => {
+    const name = req.body.name;
+    const address = req.body.address;
+    const id = req.params.id;
+
+    const locInsert = "UPDATE Locations SET `name`=?, `address`=? WHERE location_id= ?"
+    db.query(locInsert, [name, address, id], (err, result)=> {
+        if(err) return result.json(err);
+        return res.json("Location Successfully Updated") 
+    });
+});
+
+
+/************* DELETE *************/
+// Delete Volunteer Table record
 app.delete("/volunteers/:id", (req, res)=> {
     // const volID = req.params.id;
     const volID = req.params.id
@@ -52,6 +94,16 @@ app.delete("/volunteers/:id", (req, res)=> {
     db.query(delQuery, [volID], (err, result)=> {
         if(err) return result.json(err);
         return res.json("Volunteer Successfully Deleted") 
+    })
+});
+
+// Delete Location Table record
+app.delete("/locations/:id", (req, res)=> {
+    const locID = req.params.id
+    const delQuery = "DELETE FROM Locations WHERE location_id = ?"
+    db.query(delQuery, [locID], (err, result)=> {
+        if(err) return result.json(err);
+        return res.json("Location Successfully Deleted") 
     })
 });
 
