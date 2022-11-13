b5999e261b17893c8df42a49c2535fc2ac17500c
arse0039 <arse0039@umn.edu>
2022-11-13 12:04:22 -0800

    Added ShowForm component and updated volunteers

---
+++ client/src/components/ShowForm.js
@@ -0,0 +1,35 @@
+
+
+const showform = (formtype) => {
+    if (formtype == "edit") {
+        document.getElementById("insert-form").style.visibility="hidden"
+        document.getElementById("update-form").style.visibility="visible"
+        document.getElementById("delete-form").style.visibility="hidden"
+        document.getElementById("table-div").style.filter="blur(3px)"
+        document.getElementById("header").style.filter="blur(3px)"    
+        document.getElementById("search-div").style.filter="blur(3px)"
+    }else if (formtype == "delete") {
+        document.getElementById("insert-form").style.visibility="hidden"
+        document.getElementById("update-form").style.visibility="hidden"
+        document.getElementById("delete-form").style.visibility="visible"
+        document.getElementById("table-div").style.filter="blur(3px)"
+        document.getElementById("header").style.filter="blur(3px)"
+        document.getElementById("search-div").style.filter="blur(3px)"               
+    } else if (formtype == "insert") {
+        document.getElementById("insert-form").style.visibility="visible"
+        document.getElementById("update-form").style.visibility="hidden"
+        document.getElementById("delete-form").style.visibility="hidden"
+        document.getElementById("table-div").style.filter="blur(3px)"
+        document.getElementById("header").style.filter="blur(3px)" 
+        document.getElementById("search-div").style.filter="blur(3px)"
+    } else {
+        document.getElementById("insert-form").style.visibility="hidden"
+        document.getElementById("update-form").style.visibility="hidden"
+        document.getElementById("delete-form").style.visibility="hidden"
+        document.getElementById("table-div").style.filter="blur(0px)"
+        document.getElementById("header").style.filter="blur(0px)" 
+        document.getElementById("search-div").style.filter="blur(0px)"
+    }
+}
+
+export default showform;
