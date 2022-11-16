e9b2c34745dbc9c8dbce7d14c847f20e09344843
David Claphan <claphand@flip2.engr.oregonstate.edu>
2022-11-15 19:55:58 -0800

    added Activities table queries

--- server/index.js
+++ server/index.js
@@ -232,6 +232,76 @@ app.delete("/participants/:id", (req, res) => {
 });
 
 
+/***********Activities Querires (to be moved to separate file) **************/
+
+// View Activity Data for Populating Table
+app.get("/activityData", (req, res) => {
+    const activitytSelect = 'SELECT * FROM Activities';
+    db.query(activitytSelect, (err, result) => {
+        if(err) {return result.json(err)}
+        return res.json(result)
+    });
+});
+
+app.get("/activityCol", (req, res) => {
+    const activityDesc = 'Describe Activities';
+    db.query(activityDesc, (err, result) => {
+        if(err) {return result.json(err)}
+        return res.json(result)
+    });
+});
+
+
+// Insert into Activity Table
+app.post("/activitiesInsert", (req, res) => {
+    const location = req.body.location_id;
+    const volunteer = req.body.volunteer_id;
+    const ageGroup = req.body.age_group_id;
+    const name = req.body.name;
+    const description = req.body.description;
+    const maxPart = req.body.max_participants;
+
+    const activityInsert = "INSERT INTO Activities (location_id, volunteer_id, age_group_id, name, description, max_participants) VALUES (?, ?, ?, ?, ?, ?)"
+    db.query(activityInsert, [location, volunteer, ageGroup, name, description, maxPart], (err, result)=> {
+        if(err){return result.json(err)}
+        return res.json(result)
+    });
+});
+
+
+//Update Activity Table record
+app.put("activities/:id", (req, res) => {
+    const location = req.body.location_id;
+    const volunteer = req.body.volunteer_id;
+    const ageGroup = req.body.age_group_id;
+    const name = req.body.name;
+    const description = req.body.description;
+    const maxPart = req.body.max_participants;
+    const id = req.params.id;
+
+    const participantInsert = "UPDATE Participants SET `location_id`=?, `volunteer_id`=?, `age_group_id`=?, `name`=?, `description`=?, `max_participants`=? WHERE activity_id= ?"
+    db.query(participantInsert, [location, volunteer, ageGroup, name, description, maxPart, id], (err, result) => {
+        if(err) return result.json(err);
+        return res.json("Activity Successfully Updated")
+    });
+});
+
+
+// Delete Activity record
+app.delete("/activities/:id", (req, res) => {
+    const activityID = req.params.id
+    const delQuery = "DELETE FROM Activities WHERE activity_id = ?"
+    db.query(delQuery, [activityID], (err, result) => {
+        if(err) return result.json(err);
+        return res.json("Activity Successfully Deleted")
+    });
+});
+
+
+
+/********************************************/
+
+
 app.listen(PORT, ()=> {
     console.log("Connected to backend!")
 });
