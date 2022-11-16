162a4950c5985bcf14598dee4205d163e8802873
David Claphan <claphand@flip2.engr.oregonstate.edu>
2022-11-15 18:17:30 -0800

    everything above line 205 updated to connect to and work with DB

--- client/src/pages/activities.jsx
+++ client/src/pages/activities.jsx
@@ -1,8 +1,208 @@
-import React from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
 import { IonIcon } from '@ionic/react';
-import {trashOutline, buildOutline, closeCircleOutline} from 'ionicons/icons';
+import { closeCircleOutline } from 'ionicons/icons';
+import Axios from 'axios';
+import RenderTable from '../components/tableComponent';
+import showform from '../components/ShowForm';
+import RenderAgeDropdown from '../components/AgeGroupDropdown';
+import RenderLocationDropdown from '../components/LocationDropdown';
+import RenderVolunteerDropdown from '../components/VolunteerDropdown';
 
 const Activities = () => {
+
+////////////////////////////////////////////////////////////////////////////
+//// Use Effect block to populate the Activites Table from the database
+///////////////////////////////////////////////////////////////////////////
+
+    // Get data for the Activities Table
+    const [activities, setActivities] = useState([]);
+    const [renderNew, forceUpdate] = useReducer(x => x+1, 0);
+
+    useEffect(() => {
+        const getActivities = async () => {
+            try {
+                const result = await Axios.get('http://flip2.engr.oregonstate.edu:10725/activityData')
+                setActivities(result.data)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        getActivities();
+    }, [renderNew]);
+
+    // Generate Headers for Table
+    const [activityColumns, setColHeaders] = useState([])
+    let activityHeaders = []
+
+    useEffect(() => {
+        const populateHeaders = async () => {
+            try { 
+                const res = await Axios.get('http://flip2.engr.oregonstate.edu:10725/activityCol')
+                setColHeaders(res.data) 
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        populateHeaders()
+    });
+
+    const headerPop = () => {
+        activityColumns.map((e) => {
+            activityHeaders.push(e.Field)
+        })
+    }
+    headerPop()
+
+    // Get data to Generate Location Dropdown
+    const [locationDropDown, setLocationDropdown] = useState([])
+    useEffect( async () => {
+        try {
+            const result = await Axios.get('http://flip2.engr.oregonstate.edu:10725/locationData')
+            setLocationDropdown(result.data)
+        } catch (err) {
+            console.log(err)
+        }
+    }, [renderNew]);    
+
+    // Get data to Generate Volunteers Dropdown
+    const [volunteerDropDown, setVolunteerDropdown] = useState([])
+    useEffect( async () => {
+        try {
+            const result = await Axios.get('http://flip2.engr.oregonstate.edu:10725/volunteerData')
+            setVolunteerDropdown(result.data)
+        } catch (err) {
+            console.log(err)
+        }
+    }, [renderNew]);
+
+    // Get data to Generate Age Groups Dropdown
+    const [ageDropDown, setAgeDropdown] = useState([])
+    useEffect( async () => {
+        try {
+            const result = await Axios.get('http://flip2.engr.oregonstate.edu:10725/ageGroupData')
+            setAgeDropdown(result.data)
+        } catch (err) {
+            console.log(err)
+        }
+    }, [renderNew]);
+
+
+    ///////////////////////////////////////////////////////////////////
+    /// Form Block for Getting Data from user for forms/CRUD operations
+    ///////////////////////////////////////////////////////////////////
+
+    const [id, setActivityId] = useState("")
+    const [location, setActivityLocation] = useState("")
+    const [volunteer, setActivityVolunteer] = useState("")
+    const [ageGroup, setActivityAgeGroup] = useState("")
+    const [name, setActivityName] = useState("")
+    const [description, setActivityDescription] = useState("")
+    const [maxPart, setActivityMaxPart] = useState("")
+
+    const edit = (activityData) => {
+        setActivityId(activityData.activity_id)
+        setActivityLocation(activityData.location_id)
+        setActivityVolunteer(activityData.volunteer_id)
+        setActivityAgeGroup(activityData.age_group_id)
+        setActivityName(activityData.name)
+        setActivityDescription(activityData.description)
+        setActivityMaxPart(activityData.maxpart)
+        showform("edit")
+    };
+
+    const del = (activityData) => {
+        setActivityId(activityData.activity_id)
+        setActivityName(activityData.name)
+        showform("delete")
+    };
+
+    const add = () => {
+        showform("insert")
+    };
+
+    const closeForm = () => {
+        clearState()
+        showform("close")
+    };
+
+    const clearState = () => {
+        setActivityId('')
+        setActivityLocation('')
+        setActivityVolunteer('')
+        setActivityAgeGroup('')
+        setActivityName('')
+        setActivityDescription('')
+        setActivityMaxPart('')
+    };
+
+    const changeLocation = (e) => {
+        setActivityLocation(e)
+    };
+
+    const changeVolunteer = (e) => {
+        setActivityVolunteer(e)
+    };
+
+    const changeAgeGroup = (e) => {
+        setActivityAgeGroup(e)
+    };
+
+    //////////////////////////////////////////////////////
+    // CRUD Request Block
+    //////////////////////////////////////////////////////
+
+    const insertActivity = async () => {
+        try {
+            await Axios.post('http://flip2.engr.oregonstate.edu:10725/activitiesInsert', 
+            {
+                location_id: location, 
+                volunteer_id: volunteer, 
+                age_group_id: ageGroup, 
+                name: name, 
+                description: description, 
+                max_participants: maxPart
+            })
+        } catch(err){
+            console.log(err)
+        } finally {
+            closeForm()
+            clearState()
+            forceUpdate()
+        }
+    }; 
+
+    const updateActivity = async (activityID) => {
+        try {
+            await Axios.put(`http://flip2.engr.oregonstate.edu:10725/activities/${activityID}`,
+            {
+                location_id: location, 
+                volunteer_id: volunteer, 
+                age_group_id: ageGroup, 
+                name: name, 
+                description: description, 
+                max_participants: maxPart
+            })
+        } catch(err){
+            console.log(err)
+        } finally {
+            closeForm()
+            clearState()
+            forceUpdate()
+        }
+    }; 
+
+    const deleteActivity = async (activityID) => {
+        try {
+            await Axios.put(`http://flip2.engr.oregonstate.edu:10725/activities/${activityID}`)
+        } catch(err){
+            console.log(err)
+        } finally {
+            closeForm()
+            forceUpdate()
+        }
+    };
+
+    // Render the Page
     return (
         <div class="main">
             <div id="table-div">
